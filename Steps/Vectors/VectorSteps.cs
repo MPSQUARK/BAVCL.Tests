a32360c1e66637acb649a0aace27097b63009eb6
Marcel Pawelczyk <marcelpawelczyk7@gmail.com>
2023-12-16 17:25:43 +0000

    sorting out final issues

--- Steps/Vectors/VectorSteps.cs
+++ Steps/Vectors/VectorSteps.cs
@@ -1,34 +1,31 @@
-using System;
 using BAVCL.Tests.Models;
-using TechTalk.SpecFlow;
-	
-	namespace MyNamespace
+
+namespace BAVCL.Tests;
+
+[Binding]
+public class VectorSteps
+{
+	private readonly ScenarioContext _scenarioContext;
+
+	public VectorSteps(ScenarioContext scenarioContext)
 	{
-		[Binding]
-		public class VectorSteps
-		{
-			private readonly ScenarioContext _scenarioContext;
-	
-			public VectorSteps(ScenarioContext scenarioContext)
-			{
-				_scenarioContext = scenarioContext;
-			}
-			
-			[When(@"I create the following cached vector")]
-			public void GivenICreateTheFollowingCachedVector(float[] array)
-			{
-				GPU gpu = _scenarioContext.Get<GPU>("gpu");
-				Vector vector = new(gpu, array);
-				_scenarioContext.Add("vector", vector);
-			}
-			
-			[Then(@"the vector should have the following properties:")]
-			public void ThenTheVectorShouldHaveTheFollowingProperties(VectorProperties vectorProperties)
-			{
-				Vector vector = _scenarioContext.Get<Vector>("vector");
-				vectorProperties.Matches(vector);
-			}
-			
-			
-		}
+		_scenarioContext = scenarioContext;
 	}
+	
+	[When(@"I create the following cached vector")]
+	public void GivenICreateTheFollowingCachedVector(float[] array)
+	{
+		GPU gpu = _scenarioContext.Get<GPU>("gpu");
+		Vector vector = new(gpu, array);
+		_scenarioContext.Add("vector", vector);
+	}
+	
+	[Then(@"the vector should have the following properties:")]
+	public void ThenTheVectorShouldHaveTheFollowingProperties(VectorProperties vectorProperties)
+	{
+		Vector vector = _scenarioContext.Get<Vector>("vector");
+		vectorProperties.Matches(vector);
+	}
+	
+	
+}
