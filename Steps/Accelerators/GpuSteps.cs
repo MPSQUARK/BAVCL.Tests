5abe7c549579b184e8864307d15c06998d49e3e9
Marcel Pawelczyk <marcelpawelczyk7@gmail.com>
2023-12-20 15:39:39 +0000

    Add GPU caching functionality and vector properties tests

--- Steps/Accelerators/GpuSteps.cs
+++ Steps/Accelerators/GpuSteps.cs
@@ -14,8 +14,36 @@ public class GpuSteps
 	[Given(@"I have a gpu")]
 	public void GivenIhaveagpu()
 	{
-		GPU gpu = new GPU();
+		GPU gpu = new();
 		_scenarioContext.Add("gpu", gpu);
 	}
+	
+	[Then(@"it should be cached on the GPU")]
+	public void ThenItShouldBeCachedOnTheGPU()
+	{
+		GPU gpu = _scenarioContext.Get<GPU>("gpu");
+		Vector vector = _scenarioContext.Get<Vector>("vector");
+		
+		vector.ID.Should().BeGreaterThan(0);
+		gpu.IsStored(vector.ID).Should().BeTrue();
+	}
+	
+	[Then(@"there should be (\d+) items? stored on the GPU")]
+	public void ThenThereShouldBeItemsStoredOnTheGPU(int count)
+	{
+		GPU gpu = _scenarioContext.Get<GPU>("gpu");
+		gpu.StoredIDs().Count.Should().Be(count);
+	}
+	
+	[Then(@"it should not be cached on the GPU")]
+	public void ThenItShouldNotBeCachedOnTheGPU()
+	{
+		GPU gpu = _scenarioContext.Get<GPU>("gpu");
+		Vector vector = _scenarioContext.Get<Vector>("vector");
+		
+		vector.ID.Should().Be(0);
+		gpu.IsStored(vector.ID).Should().BeFalse();
+	}
+	
 
 }
--- Steps/Vectors/VectorSteps.cs
+++ Steps/Vectors/VectorSteps.cs
@@ -12,11 +12,19 @@ public class VectorSteps
 		_scenarioContext = scenarioContext;
 	}
 	
-	[When(@"I create the following cached vector")]
-	public void GivenICreateTheFollowingCachedVector(float[] array)
+	[When(@"I create the following (cached|non-cached) vector")]
+	public void GivenICreateTheFollowingCachedVector(bool isCached, float[] array)
 	{
 		GPU gpu = _scenarioContext.Get<GPU>("gpu");
-		Vector vector = new(gpu, array);
+		Vector vector = new(gpu, array, cache: isCached);
+		_scenarioContext.Add("vector", vector);
+	}
+	
+	[When(@"I create a zeros vector of length (\d+)")]
+	public void WhenICreateAZerosVectorOfLength(int length)
+	{
+		GPU gpu = _scenarioContext.Get<GPU>("gpu");
+		Vector vector = new(gpu, length);
 		_scenarioContext.Add("vector", vector);
 	}
 	
@@ -26,6 +34,38 @@ public class VectorSteps
 		Vector vector = _scenarioContext.Get<Vector>("vector");
 		vectorProperties.Matches(vector);
 	}
+
+	[Then(@"the vector should be (1|2) dimensional")]
+	public void ThenTheVectorShouldBeDimensional(int dimension)
+	{
+		Vector vector = _scenarioContext.Get<Vector>("vector");
+		if (dimension == 1)
+		{
+			//vector.Columns.Should().Be(0);
+			//vector.RowCount().Should().BeGreaterThan(0);
+		}
+			
+		
+	}
 	
+	[Then(@"the vector should have the following values on the CPU")]
+	public void ThenTheVectorShouldHaveTheFollowingValuesOnTheCPUAndGPU(float[] vectorValues)
+	{
+		Vector vector = _scenarioContext.Get<Vector>("vector");
+		vector.Value.Should().NotBeNullOrEmpty();
+		vector.Value.Should().BeEquivalentTo(vectorValues);
+	}
+	
+	[Then(@"the vector should have the following values on the GPU")]
+	public void ThenTheVectorShouldHaveTheFollowingValuesOnTheGPU(float[] vectorValues)
+	{
+		Vector vector = _scenarioContext.Get<Vector>("vector");
+		vector.SyncCPU();
+		vector.Value.Should().NotBeNullOrEmpty();
+		vector.Value.Should().BeEquivalentTo(vectorValues);
+	}
+	
+	[StepArgumentTransformation]
+	public bool IsCached(string isCached) => isCached == "cached" || string.IsNullOrEmpty(isCached);
 	
 }
