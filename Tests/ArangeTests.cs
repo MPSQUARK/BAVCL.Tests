b25c7c159d2c2dfd1a62106616d91b87df6a3eb2
marcel <marcelpawelczyk7@gmail.com>
2025-12-06 19:52:30 +0000

    Fixed tests

--- Tests/ArangeTests.cs
+++ Tests/ArangeTests.cs
@@ -2,21 +2,16 @@ namespace BAVCL.Tests.Tests;
 
 public class ArangeTests
 {
-    private readonly GPU _gpu;
-
-    public ArangeTests()
-    {
-        _gpu = new GPU();
-    }
+    public ArangeTests() { }
 
     [Fact]
     public void Arange_ShouldCreateAPositiveRangeOfNumbers()
     {
-        var result = Vector.Arange(_gpu, 0, 10, 0.5f, cache: false);
+        var result = Vector.Arange(0, 10, 0.5f);
 
         var expected = new float[] {
-            0.5f, 1.0f, 1.5f, 2.0f, 2.5f, 3.0f, 3.5f, 4.0f, 4.5f, 5.0f,
-            5.5f, 6.0f, 6.5f, 7.0f, 7.5f, 8.0f, 8.5f, 9.0f, 9.5f
+            0.0f, 0.5f, 1.0f, 1.5f, 2.0f, 2.5f, 3.0f, 3.5f, 4.0f, 4.5f,
+            5.0f, 5.5f, 6.0f, 6.5f, 7.0f, 7.5f, 8.0f, 8.5f, 9.0f, 9.5f
         };
 
         result.Should().NotBeNull();
@@ -27,7 +22,7 @@ public class ArangeTests
     [Fact]
     public void Arange_ShouldCreateANegativeRangeOfNumbers()
     {
-        var result = Vector.Arange(_gpu, 0, -10, -1f, cache: false);
+        var result = Vector.Arange(0, -10, -1f);
 
         var expected = new float[] {
             0f, -1f, -2f, -3f, -4f, -5f, -6f, -7f, -8f, -9f
@@ -37,7 +32,7 @@ public class ArangeTests
         result.Length.Should().Be(10);
         result.Should().BeEquivalentTo(expected);
 
-        var result2 = Vector.Arange(_gpu, 0, -10, 1f, cache: false);
+        var result2 = Vector.Arange(0, -10, 1f);
         result2.Should().NotBeNull();
         result2.Length.Should().Be(10);
         result2.Should().BeEquivalentTo(expected);
